67bda046bfa4495e6eb69539f37cd9cd2b15f0ee
unknown <cesar.psgjunior@gmail.com>
2023-07-19 23:48:59 -0300

    Exemplos de tipos de dados aula 1

---
+++ type_boolean.js
@@ -0,0 +1,12 @@
+//Tipos booleanos
+
+const num1 = 33;
+const num2 = 33;
+let cadastroAtivado = false;
+
+const texto1 = "Alura";
+const texto2 = "Udemy";
+
+console.log(texto1 === texto2);
+
+console.log(num1 === num2);
