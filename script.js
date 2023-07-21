2e5e9e13ac7b255241620b2f1c5241e6cda54a54
unknown <cesar.psgjunior@gmail.com>
2023-07-20 23:36:46 -0300

    Exemplos de variáveis de dados aula 1

---
+++ script.js
@@ -0,0 +1,24 @@
+//Declaração de variável com VAR
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+
+//Declaração de variavel com let
+
+let forma = 'quadrado';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if(forma === 'quadrado'){
+    console.log("Sem calculo");
+} else {
+    area = altura *  comprimento;
+    console.log(area);
+}
+
+
+
