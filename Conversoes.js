a22e9aecc0f8d56793ae5bb575293ef0ed9b349e
unknown <cesar.psgjunior@gmail.com>
2023-07-21 22:44:36 -0300

    Conversoes de formatos

---
+++ Conversoes.js
@@ -0,0 +1,24 @@
+//tipo de dado booleanos
+
+//conversao implicita
+
+const numero = 456;
+const numeroString = "456";
+
+// === iguais converte e compara
+console.log(numero === numeroString);
+
+// == dois iguais apenas comparam
+console.log(numero == numeroString);
+console.log(numero + numeroString);
+
+//Conversao explicita
+//Number() - converte string em numero
+//String() - converte numero em string
+
+console.log(numero + Number(numeroString));
+
+
+
+
+ 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//CTRL + K + C => comenta bloco de linhas
 //Declaração de variável com VAR
 
 // var altura = 5;
@@ -8,17 +9,30 @@
 
 //Declaração de variavel com let
 
-let forma = 'quadrado';
-let altura = 5;
-let comprimento = 7;
+// let forma = 'quadrado';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if(forma === 'quadrado'){
+//     console.log("Sem calculo");
+// } else {
+//     area = altura *  comprimento;
+//     console.log(area);
+// }
+
+
+//Declaração de variavel com const
+const forma =  'retangulo';
+const altura = 5;
+const comprimento = 7;
 let area;
 
-if(forma === 'quadrado'){
-    console.log("Sem calculo");
-} else {
-    area = altura *  comprimento;
-    console.log(area);
+if(forma === 'retangulo'){
+    area = altura * comprimento;
+}else {
+    area = (altura * comprimento) / 2
 }
 
-
+console.log(area);
 
