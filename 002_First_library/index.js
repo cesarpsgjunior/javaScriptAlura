df6787687c8389c9dced978793143b85f4fa72df
unknown <cesar.psgjunior@gmail.com>
2023-07-24 10:27:33 -0300

    Curso executado até promessas - primisses

--- 002_First_library/index.js
+++ 002_First_library/index.js
@@ -1,12 +1,23 @@
 import chalk from 'chalk';  //Não é nativa do JS
 import fs from 'fs';        //Nativa do JS
 
+function trataErro(erro){
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo'));
+}
+
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        console.log(chalk.green(texto))})
+        if (erro) {
+            trataErro(erro);
+        } else {
+            console.log(chalk.green(texto));
+        }
+        
+})
 }
 
-pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
 
 
