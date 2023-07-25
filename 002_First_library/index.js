e085343b85323016f0629fd033b4a4f5128652a6
unknown <cesar.psgjunior@gmail.com>
2023-07-24 22:42:30 -0300

    Promessas em JS

--- 002_First_library/index.js
+++ 002_First_library/index.js
@@ -6,17 +6,42 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'Não há arquivo'));
 }
 
-function pegaArquivo(caminhoDoArquivo){
+//Metodo utilizando async / await
+
+async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro);
-        } else {
-            console.log(chalk.green(texto));
-        }
-        
-})
+    const texto = await fs.promises.readFile
+    (caminhoDoArquivo, encoding)
+    console.log(texto);
 }
+
+
+
+
+//Método assincrono de execução da função nativo JS promises.readFile
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto)) => console.log(chalk.green(texto))
+//     .catch(trataErro)
+
+// }
+
+//Método sincrono de execução 
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         } else {
+//             console.log(chalk.green(texto));
+//         }
+        
+// })
+// }
 
 pegaArquivo('./arquivos/');
 
